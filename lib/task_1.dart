a0d25ec65f6d1bfe9d7f3cbb9a3dcbaf438f7eac
beginningshawol <bertkihi@gmail.com>
2022-01-21 15:18:36 +0900

    renamed Hello_World to Task_1

---
+++ lib/task_1.dart
@@ -0,0 +1,36 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: RandomWords(),                 // ...this line
+        ),
+      ),
+    );
+  }
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
